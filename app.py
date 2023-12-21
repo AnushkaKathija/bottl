12f1fffa3fdee563fc17fce0dec10e76095545b3
anushkakathija <anushkakathijaks@gmail>
2023-12-21 21:42:27 +0530

    bot updates

--- app.py
+++ app.py
@@ -15,22 +15,38 @@ def reply():
     responded=False
     words = incoming_msg.split('@')
     if "hello" in incoming_msg:
-        reply = "hello! \ndo ypu want to set a reminder? "
+        reply = "Welcome to BMTC ticket bot. "
+
+        reply = "Hello! \n I am your ticket-bot and I will assist you in purchasing bus tickets, bus passes and tracking live location of busses \n Type your queries or choose one of the options below for me to assist you  "
         message.body(reply)
         responded=True
-    if len(words)== 1 and "yes" in incoming_msg:
-        reminder_string="please provide date in the following format only.\n\n" "*Date @* _type the date_ "
+
+        
+
+        
+    if len(words)== 2 and "Bus Tickets" in incoming_msg:
+        reminder_string="Would you like to enter your bus stop or bus number "
         message.body(reminder_string)
         responded = True
-    if len(words) == 1 and "no" in incoming_msg:
-        reply="ok. have a nice day!"
-        message.body(reply)
-        responded =True
+        if len(words)== 2 and "Bus stop" in incoming_msg:
+            reminder_string="Please enter the source stop name choose one of the following options or type your query"
+            message.body(reminder_string)
+
+            if len(words)== 3 and "type stop name" in incoming_msg:
+                reminder_string="Please type your stop name now"
+                message.body(reminder_string)
+
+            responded = True
+        elif len(words)== 2 and "Bus number" in incoming_msg:
+            reminder_string="Enter the bus number"
+            message.body(reminder_string)
+            responded = True
+        
     elif len(words)!=1:
         input_type =words[0].strip().lower()
         input_string=words[1].strip()
         if input_type == "date":
-            reply= "please neter the reminder message in the following format only.\n\n" "*Reminder @* _type the message_"
+            reply= "please enter the reminder message in the following format only.\n\n" "*Reminder @* _type the message_"
             set_reminder_date(input_string)
             message.body(reply)
             responded=True
